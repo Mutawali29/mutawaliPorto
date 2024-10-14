da205bb5b2ff5420a1161540fc5da877e6ef34fc
unknown <qmutawali@gmail.com>
2024-10-14 21:25:31 +0700

    create root in index.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom'; 
-import App from './App.jsx'
-import './index.css'
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <StrictMode>
-    <Router>
+    <Router basename="/mutawaliPorto"> {/* Set basename here */}
       <App />
     </Router>
   </StrictMode>
